b6252c2c2154136b97aee4f75f9cf921104256d3
iambryantran <139257318+iambryantran@users.noreply.github.com>
2023-11-28 19:50:04 -0600

    added users to schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,6 +4,13 @@ CREATE DATABASE gig_work_db;
 
 USE gig_work_db;
 
+CREATE TABLE users(
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+
 CREATE TABLE workdays(
     log_date DATE NOT NULL PRIMARY KEY,
     revenue DECIMAL,
