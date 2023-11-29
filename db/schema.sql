4abc01ac6dacaba0598d9a5de32d7e31d49689c0
iambryantran <139257318+iambryantran@users.noreply.github.com>
2023-11-28 20:23:17 -0600

    change name to first_name and last_name

--- db/schema.sql
+++ db/schema.sql
@@ -6,7 +6,8 @@ USE gig_work_db;
 
 CREATE TABLE users(
     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
 );
