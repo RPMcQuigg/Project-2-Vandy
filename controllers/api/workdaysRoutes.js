dd71e1a830759d68fd2cfd5f9cafecccf3dedf05
iambryantran <139257318+iambryantran@users.noreply.github.com>
2023-12-05 20:36:01 -0600

    added workdays functions for GET requests on Rev, Exp, Net Inc, Hours, and Miles

--- controllers/api/workdaysRoutes.js
+++ controllers/api/workdaysRoutes.js
@@ -22,12 +22,100 @@ router.post('/', async (req, res) => {
 
 router.get('/', async (req, res) => {
     try {
-        const workData = await Workdays.findAll(); // Where user.id = session.user.id
-        res.status(201).json(workData);
+        const workData = await Workdays.findAll({
+            where: {
+                user_id: req.user.id
+            }
+        });
+        res.status(200).json(workData);
     } catch (err) {
         console.log(err);
-        res.status(400).json(err);
+        res.status(500).json(err);
     }
-})
+});
+
+// Lifetime
+router.get('/revenue', async (req, res) => {
+    try {
+        const totalRevenue = await Workdays.sum('revenue', {
+            where: {
+                user_id: req.user.id,
+            },
+        });
+
+        res.status(200).json({ totalRevenue });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(err);
+    }
+});
+
+router.get('/expenses', async (req, res) => {
+    try {
+        const totalRevenue = await Workdays.sum('expenses', {
+            where: {
+                user_id: req.user.id,
+            },
+        });
+
+        res.status(200).json({ totalRevenue });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(err);
+    }
+});
+
+router.get('/net-income', async (req, res) => {
+    try {
+        const totalRevenue = await Workdays.sum('revenue', {
+            where: {
+                user_id: req.user.id,
+            },
+        });
+
+        const totalExpenses = await Workdays.sum('expenses', {
+            where: {
+                user_id: req.user.id,
+            },
+        });
+
+        const netIncome = totalRevenue - totalExpenses;
+
+        res.status(200).json(netIncome);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(err);
+    }
+});
+
+router.get('/miles', async (req, res) => {
+    try {
+        const totalMiles = await Workdays.sum('miles', {
+            where: {
+                user_id: req.user.id,
+            },
+        });
+
+        res.status(200).json(totalMiles);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(err);
+    }
+});
+
+router.get('/hours', async (req, res) => {
+    try {
+        const totalHours = await Workdays.sum('hours', {
+            where: {
+                user_id: req.user.id,
+            },
+        });
+
+        res.status(200).json(totalHours);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
